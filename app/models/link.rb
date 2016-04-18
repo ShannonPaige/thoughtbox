f0562bc528bc80a7c21162fc070b29620a9833ae
ShannonPaige <spaige@gmail.com>
2016-04-18 16:03:31 -0600

    Remove read status update from model

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,12 +1,3 @@
 class Link < ActiveRecord::Base
   belongs_to :user
-
-  def update_read_status
-    if self.read_status == 'false'
-      self.read_status = 'true'
-    else
-      self.read_status = 'false'
-    end
-    self.save
-  end
 end
