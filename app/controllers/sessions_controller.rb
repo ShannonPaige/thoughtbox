20c87f727fe0b2a7caba1ac783c5dc0b6a0e3e35
ShannonPaige <spaige@gmail.com>
2016-04-25 14:38:32 -0600

    Change redirect of invalid login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       redirect_to dashboard_path
     else
       flash.now[:error] = "Invalid Login. Try Again."
-      render :new
+      render root_path
     end
   end
 
